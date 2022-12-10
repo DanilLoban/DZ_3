6bdb6f786e5fbc34f07424adb654aa9a6d9ff181
Danil <dan.loban@yandex.ru>
2022-12-10 23:32:53 +0300

    first

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите пятизначное число, ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 10000)
+{
+    int b = N / 10000;
+    int c = N % 10;
+    if(b == c)
+    {
+        N = N / 10;
+        int d = N % 1000;
+        int e1 = d / 100;
+        int e = N % 10;
+        if(e1 == e)
+        {
+            Console.Write("палиндромом");
+        }
+        else
+        {
+            Console.Write("не палиндромом");
+        }
+    }
+    else
+    {
+        Console.Write("не палиндромом");
+    }
+}
+else 
+{
+    Console.Write("введено не 5 цифр");
+}
